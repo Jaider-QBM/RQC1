6507a98a3d7598c5a3490d2c0ce78adaf7f568fa
Jaider Quimbaya <jaiderstivenquimbaya9@gmail.com>
2023-11-30 03:19:57 -0500

    Proyecto Actualizado

--- static/js/Script.js
+++ static/js/Script.js
@@ -60,9 +60,16 @@ window.onclick = function(event) {
   }
 }
 
-document.getElementById("guardarAsistencia").onclick = function() {
-  // Aquí puedes agregar el código para guardar la asistencia de los estudiantes
-  alert("Asistencia guardada");
+// Función para abrir un modal
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+}
+
+// Función para cerrar un modal
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'none';
 }
 
 
--- static/js/scroll.js
+++ static/js/scroll.js
@@ -518,4 +518,3 @@ function convertTime(time) {
 }
 
 
-
